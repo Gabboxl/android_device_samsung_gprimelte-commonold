3735bed8cef52519bf6ee3c507073734654f2143
DarkDroidDev <darkdroid.dev@gmail.com>
2020-01-14 21:58:04 +0100

    gprimelte-common: Inherit NFC and add more permissions

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,15 +1,16 @@
 # Permissions
-#PRODUCT_COPY_FILES += \
-#	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-#	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-#	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+        frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+        frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
 # NFC packages
-#PRODUCT_PACKAGES += \
-#	android.hardware.nfc@1.0-impl \
-#	android.hardware.nfc@1.0-service \
-#	com.android.nfc_extras \
-#	libnfc_nci \
-#	libnfc_nci_jni \
-#	NfcNci \
-#	Tag
+PRODUCT_PACKAGES += \
+	android.hardware.nfc@1.0-impl \
+	android.hardware.nfc@1.0-service \
+	com.android.nfc_extras \
+	libnfc_nci \
+	libnfc_nci_jni \
+	NfcNci \
+	Tag
